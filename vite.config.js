7b3f6340cc7cd8b5da856981e58ae785d2c42bbe
chido2024 <chidogodswill05@gmail.com>
2025-08-02 09:46:55 +0100

    Initil deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chido-portfolio/',
+  base: '/godswill-portfolio-react/',
   plugins: [react()],
 })
